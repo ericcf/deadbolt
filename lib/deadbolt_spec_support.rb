fe6c71e15085ed3fbce21fbf43c73673080e6611
Eric Carty-Fickes <ericcf@northwestern.edu>
2010-10-07 13:35:41 -0500

    make factory load order explicit

--- lib/deadbolt_spec_support.rb
+++ lib/deadbolt_spec_support.rb
@@ -1,2 +1,4 @@
 Dir[File.join(File.dirname(__FILE__), "../spec/support/**/*.rb")].each {|f| require f}
-Dir[File.join(File.dirname(__FILE__), "../spec/factories/**/*.rb")].each {|f| require f}
+
+require File.join(File.dirname(__FILE__), "../spec/factories/email")
+require File.join(File.dirname(__FILE__), "../spec/factories/user")
